a9c8a9af7b05a2d627f1fc2d353b4586b469c467
Ho <1501307@fcps.edu>
2020-02-20 08:58:54 -0500

    Initial commit

---
+++ app/src/main/java/com/example/layouts04/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.layouts04;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button click1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        click1 = findViewById(R.id.layout1);
+    }
+
+    public void demo1(View view) {
+        startActivity(new Intent(getApplicationContext(), ViewsInCorners1.class));
+    }
+
+    public void demo2(View view) {
+        startActivity(new Intent(getApplicationContext(), layout2.class));
+    }
+
+
+}
---
+++ app/src/main/java/com/example/layouts04/ViewsInCorners1.java
@@ -0,0 +1,14 @@
+package com.example.layouts04;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewsInCorners1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_views_in_corners1);
+    }
+}
